32acbd4525aca685c6d6ac3990cf175de523a3ec
Axel Ramada <axelramada@gmail.com>
2025-05-01 17:32:18 +0200

    Primera fusion

--- Scripts/AcomodarHijos.cs
+++ Scripts/AcomodarHijos.cs
@@ -6,11 +6,27 @@ public class AcomodarHijos : MonoBehaviour
 {
     public float alturaEntreHijos = 0.1f;
     private float escaleraOffset = -0.3f;
+    FusionManager fusionManager;
 
+
+    private void Start()
+    {
+        
+        if (FusionManager.Instance != null)
+        {
+             fusionManager = FusionManager.Instance;
+        }
+        else 
+        {
+             fusionManager = new();
+
+        }
+    }
     void Update()
     {
         if (transform.childCount > 0)
         {
+            //Ordenar a los hijos
             for (int i = 0; i < transform.childCount; i++)
             {
                 Transform hijo = transform.GetChild(i);
@@ -21,6 +37,27 @@ public class AcomodarHijos : MonoBehaviour
 
                 hijo.position = nuevaPos;
             }
+
+            //Comprobamos si solo tienen 2 hijos (ten en cuenta que hay que sumar 1 de mas por el canvas que tiene la carta)
+            if (transform.childCount == 2) 
+            {
+                //comprobar si hay que fusionar
+                //fusionManager.TryFusion();
+                GameObject hijo=null;
+                foreach (Transform child in transform)
+                {
+                    if(child.tag== "Draggable") 
+                    {
+                        hijo=child.gameObject;
+                    }
+                }
+                if (hijo != null) 
+                {
+                    fusionManager.fusionarCartas(gameObject, hijo);
+                }
+
+            }
+
         }
     }
 }
---
+++ Scripts/Carta.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Carta : MonoBehaviour
+{
+
+    public CardEnum cartaEnum;
+    public Slider slider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+    public enum CardEnum
+    {
+        Rojo,
+        Azul,
+        Morado
+    }
+}
